2be321d8a8d79032d72a267509bcb1347e5a145e
meilongli <593746221@qq.com>
2020-12-25 14:09:17 +0800

    耗材

---
+++ web_consumables/web_consumables_biz/src/test/java/com/web/consuables/biz/WebConsuablesBizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.consuables.biz;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebConsuablesBizApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
