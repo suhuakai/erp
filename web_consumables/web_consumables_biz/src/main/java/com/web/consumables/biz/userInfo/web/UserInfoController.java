81deb6b83a88974db3469900732b10c73fa47f52
meilongli <593746221@qq.com>
2020-12-28 11:11:56 +0800

    耗材

--- web_consumables/web_consumables_biz/src/main/java/com/web/consumables/biz/userInfo/web/UserInfoController.java
+++ web_consumables/web_consumables_biz/src/main/java/com/web/consumables/biz/userInfo/web/UserInfoController.java
@@ -1,8 +1,6 @@
 package com.web.consumables.biz.userInfo.web;
 
 
-import com.web.common.config.WebConfig;
-import com.web.common.config.WebListener;
 import com.web.common.security.permission.annotation.Opened;
 import com.web.consumables.api.entity.UserInfo;
 import com.web.consumables.biz.userInfo.service.UserInfoService;
@@ -10,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * <p>
