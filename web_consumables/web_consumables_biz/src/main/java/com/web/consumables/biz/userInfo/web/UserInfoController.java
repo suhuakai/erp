73e953c3844f70eb68afe636ae783e76e47b06a8
sh <18566509002@163.com>
2020-12-29 14:58:26 +0800

    框架修改

--- web_consumables/web_consumables_biz/src/main/java/com/web/consumables/biz/userInfo/web/UserInfoController.java
+++ web_consumables/web_consumables_biz/src/main/java/com/web/consumables/biz/userInfo/web/UserInfoController.java
@@ -4,6 +4,8 @@ package com.web.consumables.biz.userInfo.web;
 import com.web.common.security.permission.annotation.Opened;
 import com.web.consumables.api.entity.UserInfo;
 import com.web.consumables.biz.userInfo.service.UserInfoService;
+import com.web.core.entity.UserInfoVo;
+import com.web.core.util.LocalAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -25,8 +27,6 @@ public class UserInfoController {
 
     @Autowired
     UserInfoService userInfoService;
-
-
     /**
      * 详情
      * @param id
@@ -34,6 +34,7 @@ public class UserInfoController {
      */
     @RequestMapping("/findById")
     public UserInfo findById(String id){
+        LocalAssert.notNull(id,"ID不允许为空");
         return  userInfoService.findById(id);
     }
 
